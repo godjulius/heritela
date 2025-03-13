6b6de9c0476cb62024828afbb56219053afeca80
duongnp <duongnp28072003@gmail.com>
2025-03-13 11:13:57 +0700

    initial commit

--- src/app/components/swiper/swiper.component.ts
+++ src/app/components/swiper/swiper.component.ts
@@ -1,6 +1,8 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import {AfterViewInit, Component, CUSTOM_ELEMENTS_SCHEMA, ElementRef, ViewChild} from '@angular/core';
 import {CommonModule} from "@angular/common";
-
+import {Swiper } from 'swiper';
+import 'swiper/swiper-bundle.css';
+import {SwiperOptions} from "swiper/types";
 @Component({
   selector: 'app-swiper',
   imports: [CommonModule],
@@ -8,7 +10,8 @@ import {CommonModule} from "@angular/common";
   styleUrl: './swiper.component.css',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class SwiperComponent {
+export class SwiperComponent implements AfterViewInit{
+  @ViewChild('slider') slider!: ElementRef;
   images = [
     'https://picsum.photos/600/600?random=1',
     'https://picsum.photos/600/600?random=2',
@@ -21,4 +24,34 @@ export class SwiperComponent {
     'https://picsum.photos/600/600?random=9',
     'https://picsum.photos/600/600?random=10',
   ];
+  swiper!: Swiper;
+
+  ngAfterViewInit() {
+    const subSwiperOptions: SwiperOptions = {
+      breakpoints: {
+        640: {
+          slidesPerView: 3,
+          spaceBetween: 8,
+          slidesPerGroup: 3,
+        },
+        1024: {
+          slidesPerView: 4,
+          slidesPerGroup: 4,
+          spaceBetween: 8,
+        },
+        1440: {
+          slidesPerGroup: 5,
+          slidesPerView: 5,
+          spaceBetween: 8,
+        },
+      },
+    };
+    const subSwiperElement = document.querySelector('swiper-container.slider2');
+    Object.assign(subSwiperElement || {}, subSwiperOptions);
+    const swiperOptions: SwiperOptions = {
+
+    }
+    const swiperElement = document.querySelector('swiper-container.slider1');
+    Object.assign(swiperElement || {}, swiperOptions)
+  }
 }
